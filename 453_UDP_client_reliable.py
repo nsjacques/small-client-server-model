48dc363f8856a89aa628e103f41a82581ced814a
Noah Jacques <noah.jacques96@gmail.com>
2017-03-20 02:15:40 -0400

    All functioning, need cleaning

--- 453_UDP_client_reliable.py
+++ 453_UDP_client_reliable.py
@@ -9,15 +9,27 @@ if message.lower() != "exit":
 
 	client.sendto(message, address)
 
-	response = client.recv(1024).split(" ")
+	delay = 0.1
+	client.settimeout(0.1)
+	while delay < 2:
+		#start = time.time()
+		try:
+			response = client.recv(1024).split(" ")
+			break
+		except socket.timeout:
+			delay*=2
+		#end = time.time()
 
-	if response[0] == "200":
-		print "Status Code: ", response[0], "\nResponse: ", response[1]
 
-	elif response[0] == "300":
-		print "Status Code: ", response[0], "\nResponse: ", response[1], "\nMessage: ", " ".join(response[2:])
-	else:
-		print "This should never happen"
-		print "rep[0]", response[0], " rep[1]", response[1]
+
+	if delay < 2:
+		if response[0] == "200":
+			print "Status Code: ", response[0], "\nResponse: ", response[1]
+
+		elif response[0] == "300":
+			print "Status Code: ", response[0], "\nResponse: ", response[1], "\nMessage: ", " ".join(response[2:])
+		else:
+			print "This should never happen"
+			print "rep[0]", response[0], " rep[1]", response[1]
 
 	client.close()
